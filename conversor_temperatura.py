ab8aa962b1fa0648ea8a87803a398a963a233e87
Rodolfo Alcantara <alcantara.rodolfo97@gmail.com>
2025-04-06 12:02:35 -0300

    fix: programa agora encerra corretamente ao digitar 0

--- conversor_temperatura.py
+++ conversor_temperatura.py
@@ -3,19 +3,28 @@ def celsius_para_farenheit(celsius):
 
 def farenheit_para_celsius(farenheit):
     return (farenheit - 32) *5/9
-print("conversor de Temperatura")
-print("1 - Celsius para Farenheit")
-print("2 - Farenheit para Celsius")
 
-opcao = input("Escolha uma opção (1 ou 2):")
+while True:
+    print("conversor de Temperatura")
+    print("1 - Celsius para Farenheit")
+    print("2 - Farenheit para Celsius")
+    print("0 - Sair")
 
-if opcao == "1":
-    c = float(input("Digite a temperatura em Celsius: "))
-    f = celsius_para_farenheit(c)
-    print(f"{c}ºC equivale a {f:.2f} ºF")
-elif opcao == "2":
-    f = float(input("Digite a temperatura em Farenheit: "))
-    c = farenheit_para_celsius(f)
-    print(f"{f}ºF equivale a {c:.2f}ºC")
-else:
-    print("Opção inválida. Tente 1 ou 2")
+    opcao = input("Escolha uma opção (1, 2 ou 0):")
+
+    if opcao == "1":
+        c = float(input("Digite a temperatura em Celsius: "))
+        f = celsius_para_farenheit(c)
+        print(f"{c}ºC equivale a {f:.2f} ºF")
+
+    elif opcao == "2":
+        f = float(input("Digite a temperatura em Farenheit: "))
+        c = farenheit_para_celsius(f)
+        print(f"{f}ºF equivale a {c:.2f}ºC")
+
+    elif opcao == "0":
+        print("Saindo ... ❄️")
+        break
+
+    else:
+        print("Opção inválida. Tente 0, 1 ou 2")
