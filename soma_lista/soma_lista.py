f613566cd4e9396af9b27407653e5e8a12e3b789
Rodolfo Alcantara <alcantara.rodolfo97@gmail.com>
2025-04-08 23:36:07 -0300

    renomeando e reorganizando

--- soma_lista/soma_lista.py
+++ soma_lista/soma_lista.py
@@ -3,7 +3,7 @@ def soma_lista(lista):
     for numero in lista:
         total +=numero
     return total
-entrada = input("Digite os número da sua lista separando-os com vírgulas")
+entrada = input("Digite os número da sua lista separando-os com vírgulas: ")
 numeros = entrada.split(",")
 numeros =[int(num.strip()) for num in numeros]
 print("Soma total:", soma_lista(numeros))
